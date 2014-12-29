7ca8008cbe30b6104ad35a93ce5985e9762e74d5
yubin <yubin@yahoo-inc.com>
2014-12-28 20:16:50 -0800

    add app list

---
+++ app/views/home/app-list.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  templateName: 'home/app-list',
+
+  content: [
+{logoUrl: 'assets/img/side_icon_1.jpg', name: 'Humber Resource', number: 12},
+{logoUrl: 'assets/img/side_icon_2.jpg', name: 'Customer Relationship', number: 12},
+{logoUrl: 'assets/img/side_icon_3.jpg', name: 'Project Management', number: 12},
+{logoUrl: 'assets/img/side_icon_4.jpg', name: 'Supply Chain', number: 12},
+{logoUrl: 'assets/img/side_icon_5.jpg', name: 'Sales Force', number: 12},
+{logoUrl: 'assets/img/side_icon_6.jpg', name: 'Accounting & Tax', number: 12},
+{logoUrl: 'assets/img/side_icon_7.jpg', name: 'Analytics', number: 12},
+{logoUrl: 'assets/img/side_icon_8.jpg', name: 'e-Commerce', number: 12},
+{logoUrl: 'assets/img/side_icon_9.jpg', name: 'Highly Customer', number: 12}
+
+]
+
+});
